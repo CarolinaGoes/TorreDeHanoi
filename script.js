01261c0cb8942e4aa3d96965816f1d9cbc4cb5fa
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-04-11 11:21:04 -0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let discovermelho = document.getElementById("discovermelho");
-let discoverlaranja = document.getElementById("discoverlaranja");
-let discoveramarelo = document.getElementById("discoveramarelo");
-let discoververde = document.getElementById("discoververde");
-let discoverazul = document.getElementById("discoverazul");
+let discovermelho = document.querySelector(".discovermelho");
+let discoverlaranja = document.querySelector(".discolaranja");
+let discoveramarelo = document.querySelector(".discoamarelo");
+let discoververde = document.querySelector(".discoverde");
+let discoverazul = document.querySelector(".discoazul");
 let arrastar = false;
 let offsetX, offsetY;
 
-addEventListener("mousedown", function (e) {
-  let arrastar = true;
-  offsetX = e.clientX - elemento.offsetLeft;
-  offsetY = e.clientY - elemento.offsetTop;
-  elemento.style.cursor = "grabbing";
-});
+function tornarArrastavel(elemento) {
+  let offsetX = 0;
+  let offsetY = 0;
+  let arrastando = false;
 
-document.addEventListener("mousemove", function (e) {
+  elemento.addEventListener("mousedown", function(e) {
+      arrastando = true;
+      offsetX = e.clientX - elemento.offsetLeft;
+      offsetY = e.clientY - elemento.offsetTop;
+      elemento.style.zIndex = 1000; 
+  });
 
+  document.addEventListener("mousemove", function(e) {
+      if (arrastando) {
+          elemento.style.left = (e.clientX - offsetX) + "px";
+          elemento.style.top = (e.clientY - offsetY) + "px";
+      }
+  });
+
+  document.addEventListener("mouseup", function() {
+      arrastando = false;
+      elemento.style.zIndex = "";
+  });
 }
-
+tornarArrastavel(discovermelho);
+tornarArrastavel(discolaranja);
+tornarArrastavel(discoamarelo);
+tornarArrastavel(discoverde);
+tornarArrastavel(discoazul);
