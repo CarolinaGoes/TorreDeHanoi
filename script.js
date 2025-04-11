5b381a1e6273bb81d11fe34583cc846ffbf47de3
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-04-11 10:45:59 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 let discovermelho = document.getElementById("discovermelho");
-addEventListener("click", function() {
+let discoverlaranja = document.getElementById("discoverlaranja");
+let discoveramarelo = document.getElementById("discoveramarelo");
+let discoververde = document.getElementById("discoververde");
+let discoverazul = document.getElementById("discoverazul");
+let arrastar = false;
+let offsetX, offsetY;
 
-    discovermelho.classList.toggle("active");});
+addEventListener("mousedown", function (e) {
+  let arrastar = true;
+  offsetX = e.clientX - elemento.offsetLeft;
+  offsetY = e.clientY - elemento.offsetTop;
+  elemento.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", function (e) {
+
+}
+
