b03ac5e6af2847ea8897ae73dcfd011b27bb2fd2
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-04-14 18:27:49 -0300

    .

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let discolaranja = document.querySelector(".discolaranja");
 let discoamarelo = document.querySelector(".discoamarelo");
 let discoverde = document.querySelector(".discoverde");
 let discoazul = document.querySelector(".discoazul");
+let array1 = [discovermelho, discolaranja, discoamarelo, discoverde, discoazul]; 
+let array2 = [];
+let array3 = [];
 
 function tornarArrastavel(elemento) {
   let offsetX = 0;
@@ -14,7 +17,7 @@ function tornarArrastavel(elemento) {
     offsetX = e.clientX - elemento.offsetLeft;
     offsetY = e.clientY - elemento.offsetTop;
     elemento.style.zIndex = 1000; 
-    elemento.style.position = "absolute"; // importante para mover
+    elemento.style.position = "absolute"; 
   });
 
   document.addEventListener("mousemove", function(e) {
@@ -27,17 +30,20 @@ function tornarArrastavel(elemento) {
   document.addEventListener("mouseup", function () {
     arrastando = false;
     elemento.style.zIndex = "";
-
+  
     const posX = elemento.offsetLeft;
     const larguraJanela = window.innerWidth;
     const porcentagem = (posX / larguraJanela) * 100;
-
+  
     if (porcentagem >= 10 && porcentagem <= 20) {
-      elemento.classList.add("posição2");
-      elemento.classList.remove("posição1");
+      const index = array1.indexOf(elemento);
+      if (index !== -1) {
+        array1.splice(index, 1);
+        array2.push(elemento);
+        console.log("Elemento movido de array1 para array2");
+      }
     }
   });
-}
 
 tornarArrastavel(discovermelho);
 tornarArrastavel(discolaranja);
