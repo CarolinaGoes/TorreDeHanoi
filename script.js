7bf26cb6fd0b88bdddb962ea25b1b5f6fc721d72
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-04-17 13:29:50 -0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const disco = document.querySelector(".discovermelho");
+const disco = document.querySelector("#vermelho");
 
 
-function trocarPosicaoDiscoVermelho() {
-  
+function tornarArrastavel(elemento) {
+  let offsetX = 0;
+  let offsetY = 0;
+  let arrastando = false;
 
+  elemento.addEventListener("mousedown", function(e) {
+    arrastando = true;
+    offsetX = e.clientX - elemento.offsetLeft;
+    offsetY = e.clientY - elemento.offsetTop;
+    elemento.style.zIndex = 1000;
+  });
 
-  if (disco.classList.contains("posição6")) {
-    disco.classList.remove("posição6");
-    disco.classList.add("posição1");
-  } else {
-    disco.classList.remove("posição1");
-    disco.classList.add("posição6");
-  }
+  document.addEventListener("mousemove", function(e) {
+    if (arrastando) {
+      elemento.style.left = (e.clientX - offsetX) + "px";
+      elemento.style.top = (e.clientY - offsetY) + "px";
+    }
+  });
+
+  document.addEventListener("mouseup", function() {
+    arrastando = false;
+    elemento.style.zIndex = "";
+
+    if(const torre = document.getElementById("torreA");
+    const rect = torre.getBoundingClientRect();){
+      
+    }
+
+    disco.style.left = rect.left + "-10px";
+    disco.style.top = rect.top + "px";
+
+  });
 }
-
-trocarPosicaoDiscoVermelho();
+tornarArrastavel(disco);
